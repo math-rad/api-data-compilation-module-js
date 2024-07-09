83430df36562f9e130c3cc191807f0e7f38686bd
ben <ben.sir545@outlook.com>
2024-07-09 18:43:23 -0500

    parse url

--- parser.js
+++ parser.js
@@ -1,21 +1,5 @@
 
 class APIParser {
-    constructor(params) {
-        const storage = {}
-        for (let identifier of this.store) {
-            /**
-             * @type {string}
-             */
-            
-            storage[identifier] = identifier.endsWith("[]") ? [] : {}
-        }
-    }
-    endpoint = ""
-    store = [""]
-    paths = [{}]
-}
-
-class team extends APIParser {
     "endpoint" = "https://api.example.com/v1";
     "store" = ["dictionary", "array[]", "array2"];
     "paths" = [
@@ -35,4 +19,31 @@ class team extends APIParser {
             }
         }
     ];
+    constructor(params) {
+        const storage = {};
+        for (let identifier of this.store) {       
+            // storage object of either an array or dictionary  
+            storage[identifier] = identifier.endsWith("[]") ? [] : {};
+        };
+
+        for (let pathObject of this.paths) {
+            const pathValues = pathObject.with;
+
+            let path = pathObject.path;
+            
+            const splitPathSections = path.split('$');
+            for (let index = 0; index < Math.min(pathValues.length, splitPathSections.length); index++) {
+                const splitPathValue = pathValues[index].split('.')
+                path = path.replace('$', params[splitPathValue[0]][splitPathValue[1]], index)
+            }
+        };
+    }
+    
 }
+
+new APIParser({
+    "param": {
+        "a": "[something]",
+        "b": "[new]"
+    }
+})
